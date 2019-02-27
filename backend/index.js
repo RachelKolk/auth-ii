eb3df04936532ed656eadae257df81cc8b32fd17
RachelKolk <rach.m.obrien@gmail.com>
2019-02-27 16:44:48 -0500

    token creation and login post endpoint

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ server.get('/', (req, res) => {
     res.send("Thanks for visiting!");
 });
 
+//creates a user and hashes the password before saving it to the db
 server.post('/api/register', (req, res) => {
     let user = req.body;
 
@@ -27,6 +28,40 @@ server.post('/api/register', (req, res) => {
     Users.add(user)
       .then(saved => {
           res.status(201).json(saved);
+      })
+      .catch(err => {
+          res.status(500).json(err);
+      });
+});
+
+//the below function generates a user token for authentication
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        department: user.department,
+    };
+
+    const options = {
+        expiresIn: '1d',
+    };
+
+    return jwt.sign(payload, secret, options);
+}
+
+//use login info sent by user to authenticate the user as registered
+server.post('/api/login', (req, res) => {
+    let {username, password} = req.body;
+
+    Users.findBy({username})
+      .first()
+      .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+              const token = generateToken(user);
+              res.status(200).json(token);
+          } else {
+              res.status(401).json({message: 'You shall not pass!'});
+          }
       })
       .catch(err => {
           res.status(500).json(err);
